144e09b6d91d0442a1684adf5fdcfb1796f1a9fb
Lewis Foot <lfoot91@hotmail.co.uk>
2024-08-16 08:35:56 +0100

    feat: added server and client example projects

---
+++ PollyExampleClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+IServiceCollection services = builder.Services;
+
+services
+    .AddHttpClient("weather", client => new Uri("https:/localhost:71000"))
+    //.AddResilienceHandler();
+
+var httpClient = builder.Build().Services.GetRequiredService<IHttpClientFactory>().CreateClient();
+
